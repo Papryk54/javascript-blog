f58f692835eba272190989292ca5dee210e3e5a6
Patryk Olszewski <patrykolszewski420@gmail.com>
2024-09-24 20:19:50 +0200

    poprawka zadania 5.4

--- js/script.js
+++ js/script.js
@@ -169,35 +169,33 @@
 
 	addClickListenersToTags();
 
-//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+// function generateTags() {
+// 	/* find all articles */
+// 	const articles = document.querySelectorAll(optArticleSelector);
+// 	/* START LOOP: for every article: */
+// 	for (const article of articles) {
+// 		/* find tags wrapper */
+// 		const titleList = article.querySelector(optArticleTagsSelector);
+// 		/* make html variable with empty string */
+// 		let html = "";
+// 		/* get tags from data-tags attribute */
+// 		const articleTags = article.getAttribute("data-tags");
+// 		/* split tags into array */
+// 		const articleTagsArray = articleTags.split(" ");
 
-function generateTags() {
-	/* find all articles */
-	const articles = document.querySelectorAll(optArticleSelector);
-	/* START LOOP: for every article: */
-	for (const article of articles) {
-		/* find tags wrapper */
-		const titleList = article.querySelector(optArticleTagsSelector);
-		/* make html variable with empty string */
-		let html = "";
-		/* get tags from data-tags attribute */
-		const articleTags = article.getAttribute("data-tags");
-		/* split tags into array */
-		const articleTagsArray = articleTags.split(" ");
+// 		/* START LOOP: for each tag */
+// 		for (let tag of articleTagsArray) {
+// 			/* generate HTML of the link */
+// 			const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + "</a></li>";
+// 			/* add generated code to html variable */
+// 			html += " " + linkHTML;
+// 			/* END LOOP: for each tag */
+// 		}
+// 		/* insert HTML of all the links into the tags wrapper */
 
-		/* START LOOP: for each tag */
-		for (let tag of articleTagsArray) {
-			/* generate HTML of the link */
-			const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + "</a></li>";
-			/* add generated code to html variable */
-			html += " " + linkHTML;
-			/* END LOOP: for each tag */
-		}
-		/* insert HTML of all the links into the tags wrapper */
-
-		titleList.innerHTML = html;
-		/* END LOOP: for every article: */
-	}
-}
+// 		titleList.innerHTML = html;
+// 		/* END LOOP: for every article: */
+// 	}
+// }
 
 }
