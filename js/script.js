528a8b42e1dab8709fdf5e78833a9565b731d585
Patryk Olszewski <patrykolszewski420@gmail.com>
2024-09-27 10:00:40 +0200

    lista tagów jest generowana

--- js/script.js
+++ js/script.js
@@ -42,7 +42,8 @@
 		optTitleSelector = ".post-title",
 		optTitleListSelector = ".titles",
 		optArticleTagsSelector = ".post-tags .list",
-		optArticleAuthorSelector = ".post-author";
+		optArticleAuthorSelector = ".post-author",
+		optTagsListSelector = ".tags.list"; // chyba powinny byc oddzielone
 
 	function generateTitleLinks(customSelector = "") {
 		/* remove contents of titleList */
@@ -89,6 +90,8 @@
 	generateTitleLinks();
 
 	function generateTags() {
+		/* [NEW] create a new variable allTags with an empty array */
+		let allTags = [];
 		/* find all articles */
 		const articles = document.querySelectorAll(optArticleSelector);
 		/* START LOOP: for every article: */
@@ -108,8 +111,18 @@
 				const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + "</a></li>";
 				/* add generated code to html variable */
 				html += " " + linkHTML;
+				/* [NEW] check if this link is NOT already in allTags */
+				if(allTags.indexOf(linkHTML) == -1){
+					/* [NEW] add generated code to allTags array */
+					allTags.push(linkHTML);
+				}
 				/* END LOOP: for each tag */
 			}
+			/* [NEW] find list of tags in right column */
+  		const tagList = document.querySelector(optTagsListSelector);
+
+ 			 /* [NEW] add html from allTags to tagList */
+  		tagList.innerHTML = allTags.join(' ');
 			/* insert HTML of all the links into the tags wrapper */
 
 			titleList.innerHTML = html;
@@ -170,35 +183,6 @@
 	}
 
 	addClickListenersToTags(); //Dlaczego tu to działa 255
-
-	function generateTags() {
-		/* find all articles */
-		const articles = document.querySelectorAll(optArticleSelector);
-		/* START LOOP: for every article: */
-		for (const article of articles) {
-			/* find tags wrapper */
-			const titleList = article.querySelector(optArticleTagsSelector);
-			/* make html variable with empty string */
-			let html = "";
-			/* get tags from data-tags attribute */
-			const articleTags = article.getAttribute("data-tags");
-			/* split tags into array */
-			const articleTagsArray = articleTags.split(" ");
-
-			/* START LOOP: for each tag */
-			for (let tag of articleTagsArray) {
-				/* generate HTML of the link */
-				const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + "</a></li>";
-				/* add generated code to html variable */
-				html += " " + linkHTML;
-				/* END LOOP: for each tag */
-			}
-			/* insert HTML of all the links into the tags wrapper */
-
-			titleList.innerHTML = html;
-			/* END LOOP: for every article: */
-		}
-	}
 
 	function authorClickHandler(event) {
 		/* prevent default action for this event */
